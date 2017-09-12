6af4153e49c32444ebd348bf3d4626eee3a1bf32
naga <naga@Bop-Laptop>
2017-09-12 18:42:56 +0200

    tuto finished

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -2,8 +2,20 @@ import {
   test
 } from 'qunit';
 import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+import Ember from 'ember';
 
-moduleForAcceptance('Acceptance | list rentals');
+let StubMapsService = Ember.Service.extend({
+    getMapElement() {
+          return document.createElement('div');
+        }
+});
+
+moduleForAcceptance('Acceptance | list rentals', {
+    beforeEach() {
+          this.application.register('service:stubMaps', StubMapsService);
+          this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+        }
+});
 
 
 test('should show rentals as the home page', function(assert) {
@@ -42,4 +54,12 @@ test('should filter the list of rentals by city.', function(assert) {
   });
 });
 
-test('should show details for a selected rental', function(assert) {});
+test('should show details for a selected rental', function(assert) {
+   visit('/rentals');
+    click('a:contains("Grand Old Mansion")');
+    andThen(function() {
+          assert.equal(currentURL(), '/rentals/grand-old-mansion', 'should navigate to show route');
+          assert.equal(find('.show-listing h2').text(), "Grand Old Mansion", 'should list rental title');
+          assert.equal(find('.description').length, 1, 'should list a description of the property');
+        });
+});
